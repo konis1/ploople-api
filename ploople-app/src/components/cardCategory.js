92c21421bf0fd7634eb9569d87b462eca6d23a47
Anthony Christodoulou <christodoulou75@gmail.com>
2023-09-25 17:13:40 +0200

    added cors configuration in rails

--- ploople-app/src/components/cardCategory.js
+++ ploople-app/src/components/cardCategory.js
@@ -2,13 +2,13 @@
 import "./cardCategory.css";
 
 
-export default function CardCategory() {
+export default function CardCategory(value) {
 
   return (
     <label>
-      <input type="radio" name="categorie" value="" />
+      <input type="radio" name="categorie" value="{value.name}" />
       <div className="card-category" >
-      <h2 className="title--bottom title--center"> </h2>
+      <h2 className="title--bottom title--center">{value.name} </h2>
       </div>
     </label>
   );
--- ploople-app/src/pages/create-event-step1.js
+++ ploople-app/src/pages/create-event-step1.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import CardCategory from "../components/cardCategory"
 import BtnNext from "../components/btnNext"
-import { useNavigate } from 'react-router-dom';
 import "./create-event-step1.css";
 
 export default function App() {
@@ -9,34 +8,29 @@ export default function App() {
   const [categories, setCategories] = useState([]);
 
     const fetchCategoriesData = () => {
-      fetch("http://localhost:3000/api/version1/events/new", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': 'http://localhost:3000/api/version1/events/new',
-        }
-      })
+      fetch("http://localhost:3000/api/version1/events/new")
         .then(response => {
-          response.json();
+          return response.json();
         })
         .then(data => {
           setCategories(data);
-          console.log(data);
+          console.log(categories);
         })
     }
 
     useEffect(() => {
       fetchCategoriesData()
-    }, [categories])
+    }, [])
 
   return (
     <div className="home-container text-center">
       <form>
         <div className="steps">
           1 - Choose the type of event
+
         </div>
         <div className="cards">
-          <CardCategory />
+          <CardCategory value= {categories[0]}/>
           <CardCategory/>
           <CardCategory/>
           <CardCategory/>
