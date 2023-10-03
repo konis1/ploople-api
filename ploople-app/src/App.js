7c8d462599c879d950e601ce54ed9086396f0660
Anthony Christodoulou <christodoulou75@gmail.com>
2023-10-03 13:38:18 +0200

    added npm flatpickr

--- ploople-app/src/App.js
+++ ploople-app/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/home"
 import CreateEventStep1 from "./pages/create-event-step1"
+import CreateEventStep2 from "./pages/create-event-step2"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/create-event-step1" element={<CreateEventStep1/>}/>
+        <Route path="/create-event-step2" element={<CreateEventStep2/>}/>
       </Routes>
     </>
   );
